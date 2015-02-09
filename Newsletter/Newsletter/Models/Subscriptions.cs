6ec341bb5ad3ad539423ac46b4cc826158165deb
Frank van Driel <frank.vandriel@hotmail.com>
2015-02-09 09:40:10 +0100

    Created a model for the newsletter subscriptions

---
+++ Newsletter/Newsletter/Models/Subscriptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Newsletter.Models
+{
+    public class Subscriptions
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Interval Interval { get; set; }
+
+        [Timestamp]
+        public byte[] Created { get; set; }
+    }
+
+    public enum Interval
+    {
+        Daily = 0,
+        Weekly = 1,
+        Monthly = 2
+    }
+}
