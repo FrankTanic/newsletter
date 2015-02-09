d7a3bc41b8732a4ac38ddda2c80e3f007d4a914d
Frank van Driel <frank.vandriel@hotmail.com>
2015-02-09 10:01:21 +0100

    added a ID property to the Subscriptions model

--- Newsletter/Newsletter/Models/Subscriptions.cs
+++ Newsletter/Newsletter/Models/Subscriptions.cs
@@ -5,6 +5,7 @@ namespace Newsletter.Models
 {
     public class Subscriptions
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public Interval Interval { get; set; }
