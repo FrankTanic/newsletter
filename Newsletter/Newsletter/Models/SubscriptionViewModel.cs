6da3b8fbc87e4baceb9155cf4ebc08caec0821bb
Frank van Driel <frank.vandriel@hotmail.com>
2015-02-10 09:15:00 +0100

    Created a View model for the subscriptions

---
+++ Newsletter/Newsletter/Models/SubscriptionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Newsletter.Models
+{
+    public class SubscriptionViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please, fill in a name, so we know how to call you")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Without an emailaddress, we wont be able to send you anything")]
+        [EmailAddress(ErrorMessage = "This is not a valid emailaddress")]
+        public string Email { get; set; }
+
+        [Required]
+        public Interval Interval { get; set; }
+    }
+
+    public enum Interval
+    {
+        Daily = 0,
+        Weekly = 1,
+        Monthly = 2
+    }
+}
