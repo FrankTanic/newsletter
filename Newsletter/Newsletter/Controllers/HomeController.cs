75596de3823c5b1a59cca3ed91444019c4a4eab8
Frank van Driel <frank.vandriel@hotmail.com>
2015-02-10 09:08:15 +0100

    Created a HomeController with a ActionResult for displaying the newsletter subscribers

---
+++ Newsletter/Newsletter/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newsletter.Models;
+
+namespace Newsletter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NewsletterDbContext _db = new NewsletterDbContext();
+
+        // GET: Index
+        public ActionResult Index()
+        {
+            List<SubscriptionViewModel> subscriptions = (from s in _db.Subscriptions
+                                 select new SubscriptionViewModel
+                                 {
+                                     ID = s.ID,
+                                     Name = s.Name,
+                                     Email = s.Email,
+                                     Interval = s.Interval
+                                 }).ToList();
+
+            if(subscriptions != null)
+            {
+                return View(subscriptions);
+            }
+            return View();
+        }
+    }
+}
